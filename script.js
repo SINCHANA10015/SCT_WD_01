f667e6bde659fe686a0d2e06b63d463d48179a08
SINCHANA MUTAGEKAR <mutagekarsinchana@gmail.com>
2025-08-23 18:09:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// chnge the nav style on scroll
-window.addEventListener(scroll,function(){
-    const navbar = document.getElementById("navbar");
-    if(this.window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    }else{
-        navbar.classList.remove("scrolled");
-    }
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
-//toggle mobile menu
+
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
- menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
+window.addEventListener("scroll", function () {
+  const parallaxSection = document.querySelector("[data-parallax]");
+  let scrollPosition = window.scrollY;
+  parallaxSection.style.backgroundPositionY = scrollPosition * 0.5 + "px";
+});
